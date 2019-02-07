e7c747b748ef72c6d78deac5d7fc127e7dbeaaa9
axd18 <adrianfernandezj@hotmail.com>
2019-02-07 13:50:10 -0300

    Subida del practico 6 de febrero

---
+++ src/repasointegral6feb/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repasointegral6feb;
+
+/**
+ *
+ * @author adrian
+ */
+public class Persona {
+    private String name;
+    private String direccion;
+    private String dni;
+    
+    public void Persona(String name, String direccion, String dni){
+        this.name = name;
+        this.direccion = direccion;
+        this.dni = dni;
+    }
+    
+    
+}
