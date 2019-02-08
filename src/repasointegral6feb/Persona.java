e7c6b2be0bf429d6d6eb100d384014f3b445b509
axd18 <adrianfernandezj@hotmail.com>
2019-02-07 23:13:47 -0300

    getters y setters

--- src/repasointegral6feb/Persona.java
+++ src/repasointegral6feb/Persona.java
@@ -10,15 +10,37 @@ package repasointegral6feb;
  * @author adrian
  */
 public class Persona {
-    private String name;
+    private String nombre;
     private String direccion;
     private String dni;
     
-    public void Persona(String name, String direccion, String dni){
-        this.name = name;
+    public Persona(String name, String direccion, String dni){
+        this.nombre = nombre;
         this.direccion = direccion;
         this.dni = dni;
     }
     
+    public String getNombre(){
+        return nombre;
+    }
     
+    public String getDireccion(){
+        return direccion;
+    }
+    
+    public String getDni(){
+        return dni;
+    }
+    
+    public void setNombre(){
+        this.nombre = nombre;
+    }
+    
+    public void setDireccion(){
+        this.direccion = direccion;
+    }
+    
+    public void setDni(){
+        this.dni = dni;
+    }
 }
